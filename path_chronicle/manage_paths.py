98b85eebe16ab5e04ffee1a891f788fa13150638
Suzuki Kakeru <kakeruuuuuu0114@gmail.com>
2024-07-17 23:45:34 +0900

    clean up comment

--- path_chronicle/manage_paths.py
+++ path_chronicle/manage_paths.py
@@ -5,21 +5,21 @@ import sys
 
 class PathManager:
     def __init__(self, csv_file):
-        self.script_dir = Path(__file__).parent  # スクリプトが存在するディレクトリ
-        print(f"Script directory: {self.script_dir}")  # デバッグ出力
+        self.script_dir = Path(__file__).parent
+        print(f"Script directory: {self.script_dir}")
         self.csv_dir = self.script_dir / 'csv'
-        self.csv_dir.mkdir(exist_ok=True)  # 'csv'ディレクトリを作成
-        print(f"CSV directory: {self.csv_dir}")  # デバッグ出力
+        self.csv_dir.mkdir(exist_ok=True)
+        print(f"CSV directory: {self.csv_dir}")
         self.csv_file = self.csv_dir / csv_file
-        print(f"CSV file path: {self.csv_file}")  # デバッグ出力
-        self.paths = self.load_paths()  # self.pathsの初期化
+        print(f"CSV file path: {self.csv_file}")
+        self.paths = self.load_paths()
 
     def load_paths(self):
         paths = {}
         if not self.csv_file.exists():
-            print(f"CSV file does not exist. Creating new CSV file at {self.csv_file}")  # デバッグ出力
+            print(f"CSV file does not exist. Creating new CSV file at {self.csv_file}")
             self.paths = paths
-            self.save_paths()  # CSVファイルが存在しない場合、新規に作成
+            self.save_paths()
         else:
             with open(self.csv_file, mode='r') as file:
                 reader = csv.DictReader(file)
@@ -36,7 +36,7 @@ class PathManager:
                 writer.writeheader()
                 for name, path in self.paths.items():
                     writer.writerow({'name': name, 'path': path})
-            print(f"Paths saved to CSV file at {self.csv_file}")  # デバッグ出力
+            print(f"Paths saved to CSV file at {self.csv_file}")
         except Exception as e:
             print(f"Error saving paths: {e}", file=sys.stderr)
 
@@ -45,7 +45,7 @@ class PathManager:
             parent_path = Path(parent_dir)
             new_dir = parent_path / name
             new_dir.mkdir(parents=True, exist_ok=True)
-            print(f"Directory created at {new_dir}")  # デバッグ出力
+            print(f"Directory created at {new_dir}")
             if save_to_csv:
                 self.paths[name] = str(new_dir)
                 self.save_paths()
@@ -57,8 +57,8 @@ class PathManager:
         try:
             parent_path = Path(parent_dir)
             new_file = parent_path / name
-            new_file.touch(exist_ok=True)  # Create the file if it doesn't exist
-            print(f"File created at {new_file}")  # デバッグ出力
+            new_file.touch(exist_ok=True)
+            print(f"File created at {new_file}")
             if save_to_csv:
                 self.paths[name] = str(new_file)
                 self.save_paths()
